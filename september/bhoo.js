e5df97ec496bd5ae8e84d7fba58c0a47177ea8f5
bhoopendraa97 <bhoopendraa77@gmail.com>
2024-09-23 07:39:22 +0100

    js data

---
+++ september/bhoo.js
@@ -0,0 +1,33 @@
+
+ 
+ /*   document.getElementById("btn1").addEventListener("click" , myfun);
+function myfun(){
+ alert ("Welcome to Cybrom");
+}   */
+
+/*  document.getElementById("btn1").addEventListener("click",mycal);
+ function mycal(){
+     let physics=Number(document.getElementById("phy").value);
+     let che=Number(document.getElementById("che").value);
+     let hindi=Number(document.getElementById("hin").value);
+     let english=Number(document.getElementById("eng").value);
+     let bio=Number(document.getElementById("bio").value);
+     let totalMarks=physics+che+hindi+english+bio;
+     let per=totalMarks*100/500;
+     document.getElementById("ans1").innerHTML="Total Marks:"+totalMarks;
+     document.getElementById("ans2").innerHTML="percentage:"+per+"%";
+ }  */
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
